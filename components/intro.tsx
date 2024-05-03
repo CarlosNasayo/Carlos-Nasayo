74c6ea732c7acbd54e556e34227ab0ff12b21557
CarlosNasayo <carlosnasayov@gmail.com>
2024-05-02 22:46:29 -0500

    ficed bug

--- components/intro.tsx
+++ components/intro.tsx
@@ -56,7 +56,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Carlos.</span> I'm a{" "}
+        <span className="font-bold">Hello, I&apos;m Carlos.</span> I&apos;m a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">2 years</span> of experience. I enjoy
         building <span className="italic">apps fronted and backend</span>. My
